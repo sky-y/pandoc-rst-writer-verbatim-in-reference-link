52032374e81dc35b2a560b5895d4bc02fbb7065e
Yuki Fujiwara <sky.y.0079@gmail.com>
2020-11-01 17:21:37 +0900

    prepare for github pages

--- pandoc.sh
+++ pandoc.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pandoc src-md/verbatim-in-reference-link.md -o verbatim-in-reference-link.rst --reference-link -s
+pandoc verbatim-in-reference-link.md -o verbatim-in-reference-link.rst --reference-link -s
